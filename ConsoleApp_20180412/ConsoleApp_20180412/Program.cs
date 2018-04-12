83fc3f38285523362169aadf0d3a7bc8919f4bf5
jayfeather1255 <jayfeather1255@yahoo.com.tw>
2018-04-12 17:38:48 +0800

    不重覆隨機產生數字

--- ConsoleApp_20180412/ConsoleApp_20180412/Program.cs
+++ ConsoleApp_20180412/ConsoleApp_20180412/Program.cs
@@ -10,7 +10,29 @@ namespace ConsoleApp_20180412
     {
         static void Main(string[] args)
         {
-           
+            // 建立一個空的List
+            List<int> number = new List<int>();
+
+            // 在List裡放入1~42的數字       
+            for (int i = 1; i < 43; i++)
+            {
+                number.Add(i);
+            }
+
+            // 產生亂數物件
+            Random rand = new Random();            
+            for (int i = 0; i < 6; i++)
+            {
+                // 隨機挑選"剩下的"其中一個印出
+                int r = rand.Next(1, number.Count);
+                Console.Write(number[r]+" ");
+                
+                // 從List中刪除
+                number.RemoveAt(r);                                  
+            }
+
+            // 暫停畫面
+            Console.Read();
         }
     }
 }
